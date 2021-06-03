f89f9dca29f11edf5d91cbf8d3ccd2dffd782d83
Alisson <alisson.php@gmail.com>
2021-06-03 16:00:53 -0300

    ref(server): Segregate server setup

--- main.go
+++ main.go
@@ -1,51 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Uploader struct {
-	Version string `json:"version"`
-	Os string `json:"os"`
-	PluginId string `json:"plugin_id"`
-}
-
-func summaryHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "summary handler")
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request)  {
-
-	if r.Method != "POST" {
-		http.Error(w, "METHOD NOT ALOWED", http.StatusMethodNotAllowed)
-		return
-	}
-
-	infos := &Uploader{
-		Version: r.Header.Get("--VERSION"),
-		Os: r.Header.Get("--OS"),
-		PluginId: r.Header.Get("--PLUGIN_ID"),
-	}
-
-	w.Header().Add("Content-type", "application/json")
-	response, _ := json.Marshal(infos)
-	w.Write(response)
-	return
-}
-
-func upServer()  {
-	http.HandleFunc("/summary", summaryHandler)
-	http.HandleFunc("/upload", uploadHandler)
-
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	upServer()
+	UpServer()
 }
--- server.go
+++ server.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/alisson/go-version-manager/controllers"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"log"
+	"net/http"
+)
+
+// @title gmkernel plugins sync
+// @version 0.1
+// @description Service to upload and syncronize plugins to gmkernel flow
+// @termsOfService http://swagger.io/terms/
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @contact.name Pulse | Software Engineer
+// @contact.url https://engenharia.pulse.io
+// @contact.email engenharia@grupomateus.com.br
+
+// UpServer @host localhost:8000
+// @BasePath /
+func UpServer()  {
+	http.HandleFunc("/summary", controllers.Index)
+	http.HandleFunc("/upload", controllers.Upload)
+	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
