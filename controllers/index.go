be28f326e7a88676b02448536c03428543fe069f
Alisson <alisson.php@gmail.com>
2021-06-04 17:43:06 -0300

    feat(summary): WIṔ: implement handler

--- controllers/index.go
+++ controllers/index.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"net/http"
 )
-
+// Index godoc
+// @Summary List all plugins
+// @Tags sync
+// @Description Retrieve list plugin with all versions
+// @Produce json
+// @Success 200 {array} Uploader
+// @Router / [get]
 func Index(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "summary handler")
+	_, err := fmt.Fprintf(w, "summary handler")
+	if err != nil {
+		return
+	}
 }
